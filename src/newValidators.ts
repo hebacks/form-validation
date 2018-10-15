71837518bb2a72385a737bd865d756b0de7fbb14
Gosia Heba <malgorzataheba@gmail.com>
2018-10-16 01:33:35 +0200

    add types to new validators

--- src/newValidators.ts
+++ src/newValidators.ts
@@ -1,14 +1,17 @@
-export const hasCapitalLetter = () => value => {
-  let hasCapitalLetter = [...value].find(
-    letter => letter === letter.toUpperCase() && isNaN(letter)
-  );
+interface ValidationResut {
+  isValid: boolean;
+  errorMessage: string;
+}
+
+export const hasCapitalLetter = () => (value: any): ValidationResut => {
+  const capitalLetterRegex = /[A-Z]/;
   return {
-    isValid: hasCapitalLetter !== undefined,
+    isValid: capitalLetterRegex.test(value),
     errorMessage: "This value has to contain at least one capital letter. "
   };
 };
 
-export const hasNumber = () => value => {
+export const hasNumber = () => (value: any): ValidationResut => {
   let hasNumber = [...value].find(letter => letter.trim() && !isNaN(letter));
   return {
     isValid: hasNumber !== undefined,

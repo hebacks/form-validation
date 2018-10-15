5b1770e2de2d5be11a392bcbcc5fc2f2e525059c
Gosia Heba <malgorzataheba@gmail.com>
2018-10-16 01:20:17 +0200

    add sass-loader and ExtractTextPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractPlugin = new ExtractTextPlugin({
+  filename: "main.css"
+});
 
 module.exports = {
   entry: "./src/index.js",
@@ -16,9 +20,16 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ["css-loader", "sass-loader"]
+        })
+      },
+      {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"]
       }
     ]
-  }
+  },
+  plugins: [extractPlugin]
 };

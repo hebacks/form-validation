4323b4278fa45da547d3cc45a49c632b11ebde95
Gosia Heba <malgorzataheba@gmail.com>
2018-11-13 00:35:07 +0100

    added initial karma config

---
+++ karma.conf.js
@@ -0,0 +1,66 @@
+// Karma configuration
+
+module.exports = function(config) {
+  config.set({
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: "",
+
+    frameworks: ["jasmine"],
+
+    plugins: ["karma-jasmine", "karma-webpack", "karma-chrome-launcher"],
+
+    files: [
+      { pattern: "test/*.spec.js", watched: false },
+      { pattern: "test/**/*.spec.js", watched: false }
+      // each file acts as entry point for the webpack configuration
+    ],
+
+    preprocessors: {
+      "test/*.spec.js": ["webpack"],
+      "test/**/*.spec.js": ["webpack"]
+    },
+
+    webpack: {
+      resolve: {
+        extensions: [".ts", ".js"]
+      },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            use: "ts-loader",
+            exclude: /node_modules/
+          }
+        ]
+      }
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ["progress"],
+
+    // web server port
+    port: 9876,
+
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // start these browsers
+    browsers: ["Chrome"],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  });
+};

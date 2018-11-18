e6264fd121d6be6b75e571d56fed4463ff1d58e0
Gosia Heba <malgorzataheba@gmail.com>
2018-11-18 21:36:12 +0100

    updated karma config

--- karma.conf.js
+++ karma.conf.js
@@ -50,7 +50,7 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
     // start these browsers
     browsers: ["ChromeHeadless"],
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous

7b59cc84d6785c9ba2191ac17c6a71a5f507c403
Gosia Heba <malgorzataheba@gmail.com>
2018-11-18 19:38:15 +0100

    added loader to an input with async validation

--- src/async.js
+++ src/async.js
@@ -1,8 +1,10 @@
-export const isUnique = errorMessage => value => {
+export const isUnique = errorMessage => (value, field) => {
   const usernames = ["Baks", "Baksik", "HebX"];
+  field.element.parentElement.classList.add("loader");
   return new Promise(resolve => {
     setTimeout(() => {
       let isTaken = usernames.indexOf(value) > -1;
+      field.element.parentElement.classList.remove("loader");
       resolve({
         isValid: !isTaken,
         errorMessage: errorMessage
